3ebd646d6cafc3b05264443811d7f879d2943aab
Calista <calistasalsabila313@gmail.com>
2025-07-09 09:49:44 +0700

    add main function

---
+++ Dart/code/dart_basic/bin/34_main_function.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  print("Argument $args");
+}
+
+// output
+// Argument []
+
+// (base) PS E:\VsCode\GitHub\Programming-Languages\Dart> dart code/dart_basic/bin/34_main_function.dart calista informatika atau kasi petik dua 
+// Argument [calista, informatika]

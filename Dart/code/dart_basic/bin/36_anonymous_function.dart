83ac8d1b1efa7d741034a6a495949f78ca7657b1
Calista Salsabila <calistasalsabila313@gmail.com>
2025-07-15 09:57:04 +0700

    add anonymous function

--- Dart/code/dart_basic/bin/36_anonymous_function.dart
+++ Dart/code/dart_basic/bin/36_anonymous_function.dart
@@ -1,3 +1,8 @@
+import 'dart:ffi';
+
+void hi(String name, String Function (String) filter){
+  print('Hi ${filter(name)}');
+}
 void main(){
 
   var upperFunction = (String name){
@@ -7,7 +12,12 @@ void main(){
 
 var lowerFunction = (String name) => name.toLowerCase();
 
-
   print(upperFunction('cal'));
   print(lowerFunction('CAL'));
+
+  hi('Dokja', (name){
+    return name.toUpperCase();
+  });
+
+  hi('Jeha', (name)=> name.toLowerCase());
 }

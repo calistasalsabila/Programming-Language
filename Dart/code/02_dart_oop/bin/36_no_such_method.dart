de6585e40789e5305ac285fc64503aec2589cf74
Calista <calistasalsabila313@gmail.com>
2025-08-24 15:15:50 +0700

    add no such method

--- Dart/code/02_dart_oop/bin/36_no_such_method.dart
+++ Dart/code/02_dart_oop/bin/36_no_such_method.dart
@@ -1,11 +1,17 @@
 import 'dart:mirrors';
 
+abstract class CategoryRepo {
+  name(String name);
+  id(String id);
+  quantity(int quantity);
+}
 
-
-class Repo  {
+class Repo extends CategoryRepo {
   final String _name;
   Repo(this._name);
 
+  // It is not mandatory to implement name, id, and quantity explicitly because
+  // all unimplemented method calls will be intercepted by noSuchMethod.
   @override
   dynamic noSuchMethod(Invocation invocation) {
     var column = MirrorSystem.getName(invocation.memberName);
@@ -16,14 +22,18 @@ class Repo  {
   }
 }
 
-
-
 void main() {
   dynamic repo = Repo("one");
   repo.id(1);
+
+  // Error if not implemented in abstract class
+  // Works only if the class is not abstract
+  // repo.hahahha(1);
+
+
 }
 
 // output
 // SELECT * FROM one WHERE id = '1'
 
-
+// See the previous commit for not implemented abstract class

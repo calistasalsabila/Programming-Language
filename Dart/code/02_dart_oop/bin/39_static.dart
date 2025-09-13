a8125962fbe1abbb0a91e7b04f44d585b6115243
calista <calistasalsabila313@gmail.com>
2025-09-13 07:29:57 +0700

    add static

---
+++ Dart/code/02_dart_oop/bin/39_static.dart
@@ -0,0 +1,14 @@
+class Novel {
+
+  static final String mainCharacter = "Kim Dokja";
+  static final String title = "Demon King of Salvation";
+
+}
+
+
+void main(){
+
+  print(Novel.mainCharacter);
+  print(Novel.title);
+  
+}

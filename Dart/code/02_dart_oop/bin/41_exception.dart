ef7d7aea15cef259cd8f8e5bd65b5a31c786369a
calista <calistasalsabila313@gmail.com>
2025-09-20 06:53:43 +0700

    update exception

--- Dart/code/02_dart_oop/bin/41_exception.dart
+++ Dart/code/02_dart_oop/bin/41_exception.dart
@@ -7,6 +7,8 @@ class Validation{
 
     }else if(password == ' '){
       throw ValidationException("Password is blank");
+    }else if(username != 'calista' || password != 'heheh'){
+      throw Exception("Login Failed!");
     }
     // valid
   }
@@ -24,10 +26,24 @@ class ValidationException implements Exception{
 void main(){
 
   try{
-    Validation.validate(' ', ' ');
-  }on ValidationException {
-    print("Validation Error");
+    Validation.validate('dokja', ',');
+  }on ValidationException  catch (exception) {
+    print("Validation Error and Error : ${exception.message}");
+  }on Exception catch (exception){
+    print("${exception.toString()}");
+  }finally{
+    print("ok");
   }
 
   print('selesai');
+
+  // atau bisa gini
+  try{
+    Validation.validate('dokja', ',');
+  }catch (exception) {
+    print("Error : ${exception.toString()}");
+  }finally{
+    print("ok");
+  }
+
 }

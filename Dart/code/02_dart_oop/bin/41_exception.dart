6ad75c2560f3939d42e3eacf2a5ad8e5f764f88d
calista <calistasalsabila313@gmail.com>
2025-09-20 06:38:27 +0700

    add exception

---
+++ Dart/code/02_dart_oop/bin/41_exception.dart
@@ -0,0 +1,33 @@
+class Validation{
+
+  static void validate(String username, String password){
+
+    if(username==' '){
+      throw ValidationException("Username is blank");
+
+    }else if(password == ' '){
+      throw ValidationException("Password is blank");
+    }
+    // valid
+  }
+
+}
+
+class ValidationException implements Exception{
+  String message;
+
+  ValidationException(this.message);
+
+}
+
+
+void main(){
+
+  try{
+    Validation.validate(' ', ' ');
+  }on ValidationException {
+    print("Validation Error");
+  }
+
+  print('selesai');
+}

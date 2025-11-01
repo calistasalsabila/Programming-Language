beb5ec17c216cca3ae21f75a7f2a8514a77b3733
calista <calistasalsabila313@gmail.com>
2025-11-01 14:04:53 +0700

    add funct

---
+++ JavaScript/01_js_basic/code/08_function.js
@@ -0,0 +1,47 @@
+function hi(){
+    console.log("hi");
+}
+
+
+// function expression
+const hello = function hehehe(){
+    console.log("Hello");
+}
+
+hello();
+
+// function as argument
+function hm(){
+    console.log('hm');
+}
+
+function doublee(func){
+    func();
+    func();
+}
+
+doublee(hm);
+
+// function return function
+
+function angkaRand(){
+    const rand = Math.random();
+    console.log(rand);
+
+    if(rand > 0.5){
+        return function yeay(){
+            console.log("u got more than 0.5");
+        }
+    }else{
+        return function yeay2(){
+            console.log("u got less than 0.5");
+        }
+    }
+
+}
+
+// cara manggilnya
+angkaRand()();
+
+const result = angkaRand();
+result();

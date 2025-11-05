1ba7d7e3a651d07f690e0be48d181981e91ba5a4
calista <calistasalsabila313@gmail.com>
2025-11-05 13:17:22 +0700

    add arrow func

---
+++ JavaScript/01_js_basic/code/18_arr_func.js
@@ -0,0 +1,9 @@
+const ROARRRRRRR = {
+    firstName: "Dino",
+    lastName: "Saurus",
+    fullname: function (){
+        return `${this.firstName}${this.lastName}`;
+    },
+};
+
+console.log(ROARRRRRRR.fullname());

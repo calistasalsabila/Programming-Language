15d05af845619f50b25b4e8c0ff1f9f8fa4b7428
calista <calistasalsabila313@gmail.com>
2025-11-09 10:39:53 +0700

    add const func

---
+++ JavaScript/02_js_oop/code/04_object_class.js
@@ -0,0 +1,28 @@
+class Color {
+    constructor(r, g, b, name){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+    }
+
+    innerRgb(){
+        const {r,g,b} = this;
+        return `${r}, ${g}, ${b}` 
+    }
+
+    color(){
+        console.log("the color is " + this.name);
+        return "huahahahhaa"
+    }
+    rgb() {
+        return `rgb(${this.innerRgb()})` 
+    }
+
+}
+
+
+const blue = new Color(0,0,255, "blue");
+
+console.log(blue.color());
+console.log(blue.rgb());

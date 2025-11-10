d9d2d6043c68db80d8e59932ef30026cc5c7bdbf
calista <calistasalsabila313@gmail.com>
2025-11-10 15:03:37 +0700

    update

---
+++ JavaScript/03_js_asynchronus/code/02_asyncwait/app.js
@@ -0,0 +1,11 @@
+setTimeout(function(){
+    document.body.style.backgroundColor = 'blue';
+    
+    setTimeout(function(){
+        document.body.style.backgroundColor = 'green';
+    }, 4000)
+
+}, 3000)
+
+
+

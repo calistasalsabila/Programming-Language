da93f88bb0f079bdd02fc3bac916994461d99e2e
calista <calistasalsabila313@gmail.com>
2025-11-10 22:46:55 +0700

    study await

--- JavaScript/03_js_asynchronus/code/04_background_changer/app.js
+++ JavaScript/03_js_asynchronus/code/04_background_changer/app.js
@@ -11,3 +11,14 @@ const delayBackgroundChanger = (color, delay) => {
 delayBackgroundChanger('blue', 2000)
     .then(() => delayBackgroundChanger('green', 4000))
     .then(() => delayBackgroundChanger('purple', 6000));
+
+
+// 06 await
+async function changeColor() {
+    await delayBackgroundChanger('blue', 2000);
+    await delayBackgroundChanger('green', 2000);
+    return "all done~"
+}
+
+
+changeColor().then((res) => {alert(res)});

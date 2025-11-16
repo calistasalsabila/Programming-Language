91b65c66e6622c1e8c73ebca8cf3f92d3195b3e4
calista <calistasalsabila313@gmail.com>
2025-11-16 10:50:15 +0700

    rev

--- JavaScript/04_js_dom/src/cp-javascript-dom/selection/app.js
+++ JavaScript/04_js_dom/src/cp-javascript-dom/selection/app.js
@@ -13,6 +13,8 @@ for (let img of squareImages) {
 		'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/1920px-Cat_August_2010-4.jpg';
 }
 
+
+
 const links = document.querySelectorAll('a');
 
 for (let link of links) {

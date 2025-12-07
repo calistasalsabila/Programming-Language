bca236c80e27c3233a8d8af9b6b5f5d837ad563b
calista salsabila <calistasalsabila313@gmail.com>
2025-12-07 19:57:06 +0700

    add generate random color

--- JavaScript/04_js_dom/code/01_event/app.js
+++ JavaScript/04_js_dom/code/01_event/app.js
@@ -16,3 +16,27 @@ const again = function (){
 }
 
 button.onmouseover = again;
+
+// addeventlistener
+const eventbtn = document.querySelector('#eventbtn');
+
+eventbtn.addEventListener('click', function(){
+    alert('clicked');
+})
+
+
+function stepOne (){
+    console.log("step one");
+}
+
+function stepTwo (){
+    console.log("step two");
+}
+
+// on click, ketimpa variable terbaru
+eventbtn.onclick = stepOne;
+eventbtn.onclick = stepTwo; // -> ni outputnya <nimpa yg 1>
+
+// biar ga ketumpuk
+eventbtn.addEventListener('click', stepOne);
+eventbtn.addEventListener('click', stepTwo);

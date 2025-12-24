35033ed640a1210589afc4f50c2a787152b2c578
calista salsabila <calistasalsabila313@gmail.com>
2025-12-24 10:14:20 +0700

    add input change

--- JavaScript/04_js_dom/code/05_event_preventdefault/app.js
+++ JavaScript/04_js_dom/code/05_event_preventdefault/app.js
@@ -1,7 +1,24 @@
 const form = document.querySelector('form');
+const input = document.querySelector('input');
+const list = document.querySelector('#notes');
+
+input.addEventListener('change' , (e) => {
+    console.log('nilai input berubah');
+})
+
+input.addEventListener('input' , (e) => {
+    console.log('nilai input berubah');
+})
 
 form.addEventListener('submit', function(e){
     // aksi otomatis browser dibatalkan
     e.preventDefault();
-    console.log("sumbitted");
-})
+    const noteValue = input.value;
+    const newList = document.createElement('li');
+    newList.innerText = noteValue;
+    list.append(newList);
+    console.log(noteValue);
+    input.value = '';
+});
+
+
